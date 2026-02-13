847e2ad3e8ae49a06b344a1e8ccae08c8ec9e9ac
haileysunn <haileysunn.o3o@gmail.com>
2026-02-13 10:50:29 +0900

    fix: KakaoMap에도 services 라이브러리 추가

--- components/KakaoMap.tsx
+++ components/KakaoMap.tsx
@@ -42,7 +42,7 @@ export default function KakaoMap({
     if (!mapRef.current) return;
 
     const script = document.createElement('script');
-    script.src = `https://dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_MAP_KEY}&autoload=false`;
+    script.src = `https://dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_MAP_KEY}&libraries=services&autoload=false`;
     script.async = true;
     document.head.appendChild(script);
 
