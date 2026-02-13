cd593ce35a1e4bcac39a20b2a6ecf86c44fa038d
haileysunn <haileysunn.o3o@gmail.com>
2026-02-13 10:48:03 +0900

    fix: Kakao SDK URL에 https 프로토콜 명시

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
     <html lang="ko">
       <body>
         <Script
-          src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_MAP_KEY}&libraries=services&autoload=false`}
+          src={`https://dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_MAP_KEY}&libraries=services&autoload=false`}
           strategy="beforeInteractive"
         />
         <AuthProvider>{children}</AuthProvider>
