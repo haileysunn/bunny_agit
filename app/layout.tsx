02926529d751e3d15c23b704c2a45ce944fa6af7
haileysunn <haileysunn.o3o@gmail.com>
2026-02-13 10:24:08 +0900

    fix: Kakao Maps SDK에 services 라이브러리 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ko">
+      <head>
+        <script
+          type="text/javascript"
+          src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_MAP_KEY}&libraries=services&autoload=false`}
+        />
+      </head>
       <body>
         <AuthProvider>{children}</AuthProvider>
       </body>
